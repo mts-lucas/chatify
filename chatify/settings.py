de54ac0f8483aeb4f9cd3300d5361bf6a4402dc2
Lucas Mateus <lmateus1067@outlook.com>
2023-10-30 14:55:54 -0300

    feat: add models and migrations #3

--- chatify/settings.py
+++ chatify/settings.py
@@ -39,8 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
-    'chat',
+    'api.apps.ApiConfig',
+    'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
