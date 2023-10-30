8049efc77a8bf0367e59f770a5a69088c1f9cea0
Lucas Mateus <lmateus1067@outlook.com>
2023-10-30 20:50:49 -0300

    conf: add layer conf #3

--- chatify/settings.py
+++ chatify/settings.py
@@ -75,7 +75,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'chatify.wsgi.application'
 ASGI_APPLICATION = 'chatify.asgi.application'
 
-
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
