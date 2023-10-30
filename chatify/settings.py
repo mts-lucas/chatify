d7d2ee0fc65f92e429083ddbf1a1b371851ec992
Lucas Mateus <lmateus1067@outlook.com>
2023-10-30 09:45:58 -0300

    ref: rename app #3

--- chatify/settings.py
+++ chatify/settings.py
@@ -38,9 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'channels'
     'api',
-    'messages',
+    'chat',
 ]
 
 MIDDLEWARE = [
