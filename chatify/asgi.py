50b11ac72890872d5e9552ec37e964487fe9f985
Lucas Mateus <lmateus1067@outlook.com>
2023-10-30 21:04:16 -0300

    feat: add ws routes #3

--- chatify/asgi.py
+++ chatify/asgi.py
@@ -9,14 +9,16 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatify.settings')
 
-application = ProtocolTypeRouter(
-    {
-        "http": get_asgi_application(),
-        # Just HTTP for now. (We can add other protocols later.)
-    }
-)
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': URLRouter(
+        chat.routing.websocket_urlpatterns
+    ),
+})
